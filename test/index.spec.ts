4d8bb86167373c0444423220a3f70054e6a32339
blck-snwmn <whitesnowmancreator@gmail.com>
2024-03-24 15:45:10 +0900

    Update test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,10 +1,10 @@
-import { afterEach, describe, expect, it, vi } from "vitest";
-import worker from "../src/worker";
 import {
 	createExecutionContext,
 	env,
 	waitOnExecutionContext,
 } from "cloudflare:test";
+import { afterEach, describe, expect, it, vi } from "vitest";
+import worker from "../src/worker";
 
 const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
 
@@ -17,34 +17,98 @@ describe("Hello World worker", () => {
 		// Intercept calls to `QUEUE_PRODUCER.send()`
 		const sendSpy = vi
 			.spyOn(env.SQUEUE, "send")
-			.mockImplementation(async () => { });
+			.mockImplementation(async () => {});
+
+		const now = new Date();
 
 		// Enqueue job on queue
 		const ctx = createExecutionContext();
-		const response = await worker.tail([
-			{
-				// TraceItemFetchEventInfo
-				event: {
-					request: new IncomingRequest("https://example.com", {}),
-					response: new Response("ok"),
+		const response = await worker.tail(
+			[
+				{
+					// TraceItemFetchEventInfo
+					event: {
+						request: new IncomingRequest("https://example.com", {}),
+						response: new Response("ok"),
+					},
+					scriptName: "scriptName_test",
+					outcome: "ok",
+					exceptions: [],
+					eventTimestamp: now.getTime(),
+					logs: [],
+					diagnosticsChannelEvents: [],
 				},
-				scriptName: "test",
-				outcome: "ok",
-				exceptions: [],
-				eventTimestamp: new Date().getTime(),
-				logs: [],
-				diagnosticsChannelEvents: [],
-			},
-		], env, ctx);
+			],
+			env,
+			ctx,
+		);
 		await waitOnExecutionContext(ctx);
 
 		// Check `QUEUE_PRODUCER.send()` was called
 		expect(sendSpy).toBeCalledTimes(1);
-		// expect(sendSpy).toBeCalledWith({
-
-		// });
+		expect(sendSpy).toBeCalledWith({
+			type: "chat.postMessage",
+			body: {
+				channel: env.NOTIFY_CHANNEL,
+				attachments: [
+					{
+						color: "#28a745",
+						blocks: [
+							{
+								type: "header",
+								text: {
+									type: "plain_text",
+									text: "Worker execution",
+								},
+							},
+							{
+								type: "section",
+								fields: [
+									{
+										type: "mrkdwn",
+										text: "*ScriptName:*\nscriptName_test",
+									},
+									{
+										type: "mrkdwn",
+										text: `*EventAt:*\n${now.toLocaleString("ja-JP", {
+											timeZone: "Asia/Tokyo",
+										})}`,
+									},
+								],
+							},
+							{
+								type: "section",
+								fields: [
+									{
+										type: "mrkdwn",
+										text: "*Event:*\nFetch",
+									},
+									{
+										type: "mrkdwn",
+										text: "*Outcome:*\nok",
+									},
+								],
+							},
+							{
+								type: "section",
+								fields: [
+									{
+										type: "mrkdwn",
+										text: "*HTTP Status:*\n200",
+									},
+									{
+										type: "mrkdwn",
+										text: "*Exceptions:*\nn/a",
+									},
+								],
+							},
+						],
+					},
+				],
+			},
+		});
 	});
-})
+});
 
 // it("produces queue message with mocked consumer", async () => {
 // 	const consumerSpy = vi
